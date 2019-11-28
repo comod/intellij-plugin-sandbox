530a33d6c463a7cf379e5b7aa6afd248883ab33a
Michael Wölk <michael.woelk@check24.de>
2019-11-28 20:05:41 +0100

    fileStatusesChanged-Listener

--- src/main/java/system/ProjectStartListener.java
+++ src/main/java/system/ProjectStartListener.java
@@ -2,11 +2,29 @@ package system;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.vcs.FileStatusListener;
+import com.intellij.openapi.vcs.FileStatusManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
 
 public class ProjectStartListener implements ProjectManagerListener {
 
     public void projectOpened(Project project) {
+
         System.out.println("projectOpened (ProjectManagerListener)" + project.toString());
+
+        FileStatusManager.getInstance(project).addFileStatusListener(new FileStatusListener() {
+            @Override
+            public void fileStatusesChanged() {
+                System.out.println("fileStatusesChanged");
+            }
+
+            @Override
+            public void fileStatusChanged(@NotNull VirtualFile virtualFile) {
+                System.out.println("fileStatusChangedVirtualFile");
+            }
+        });
+
     }
 
     public void projectClosed(Project project) {
